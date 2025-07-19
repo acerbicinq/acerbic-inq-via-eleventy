0f9fe1be168a53a722623f5ee90769df1c488aef
GlisteningMopeds <60531638+acerbicinq@users.noreply.github.com>
2025-07-19 16:26:53 -0700

    Finishing with eleventy
    I forgot what was done, honestly. But committing it.

--- .eleventy.js
+++ .eleventy.js
@@ -17,3 +17,29 @@ return {
     }
 };
 }
+
+
+
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
+
+export default function (eleventyConfig) {
+	eleventyConfig.addPlugin(feedPlugin, {
+		type: "atom", // or "rss", "json"
+		outputPath: "/feed.xml",
+		collection: {
+			name: "posts", // iterate over `collections.posts`
+			limit: 10,     // 0 means no limit
+		},
+		metadata: {
+			language: "en",
+			title: "Blog Title",
+			subtitle: "This is a longer description about your blog.",
+			base: "https://example.com/",
+			author: {
+				name: "Your Name",
+				email: "", // Optional
+			}
+		}
+	});
+};
+
